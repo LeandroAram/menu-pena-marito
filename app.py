52d35d8be6f4779575c276a12e2455583d4d81ef
Pedro Aramayo <44516486@populorumjujuy.ar>
2026-05-21 11:13:55 -0300

    agregando categorias

--- app.py
+++ app.py
@@ -15,6 +15,34 @@ def menu_es():
 def regionales():
     return render_template('regionales.html')
 
+@app.route('/tradicionales')
+def tradicionales():
+    return render_template('tradicionales.html')
+
+@app.route('/pastas')
+def pastas():
+    return render_template('pastas.html')
+
+@app.route('/entradas')
+def entradas():
+    return render_template('entradas.html')
+
+@app.route('/postres')
+def postres():
+    return render_template('postres.html')
+
+@app.route('/bebidas')
+def bebidas():
+    return render_template('bebidas.html')
+
+@app.route('/cervezas')
+def cervezas():
+    return render_template('cervezas.html')
+
+@app.route('/vinos')
+def vinos():
+    return render_template('vinos.html')
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
