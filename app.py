3cf576a175ba6df51600f4d0e0f8add82677da9a
Pedro Aramayo <44516486@populorumjujuy.ar>
2026-05-23 14:40:42 -0300

    e

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 def inicio():
     return render_template('index.html')
 
+# =========================
 # ESPAÑOL
+# =========================
 
 @app.route('/menu_es')
 def menu_es():
@@ -37,35 +39,101 @@ def postres():
 def menu_dia():
     return render_template('menu-dia.html')
 
+# =========================
 # INGLES
+# =========================
 
 @app.route('/menu_en')
 def menu_en():
     return render_template('menu_en.html')
 
-@app.route('/starters_en')
-def starters_en():
+@app.route('/regionales_en')
+def regionales_en():
+    return render_template('regionales_en.html')
+
+@app.route('/tradicionales_en')
+def tradicionales_en():
+    return render_template('tradicionales_en.html')
+
+@app.route('/pastas_en')
+def pastas_en():
+    return render_template('pastas_en.html')
+
+@app.route('/entradas_en')
+def entradas_en():
     return render_template('starters_en.html')
 
+@app.route('/postres_en')
+def postres_en():
+    return render_template('postres_en.html')
+
+@app.route('/menu-dia_en')
+def menu_dia_en():
+    return render_template('menu-dia_en.html')
+
+# =========================
 # PORTUGUES
+# =========================
 
 @app.route('/menu_pt')
 def menu_pt():
     return render_template('menu_pt.html')
 
+@app.route('/regionales_pt')
+def regionales_pt():
+    return render_template('regionales_pt.html')
+
+@app.route('/tradicionales_pt')
+def tradicionales_pt():
+    return render_template('tradicionales_pt.html')
+
+@app.route('/pastas_pt')
+def pastas_pt():
+    return render_template('pastas_pt.html')
+
 @app.route('/entradas_pt')
 def entradas_pt():
     return render_template('entradas_pt.html')
 
+@app.route('/postres_pt')
+def postres_pt():
+    return render_template('postres_pt.html')
+
+@app.route('/menu-dia_pt')
+def menu_dia_pt():
+    return render_template('menu-dia_pt.html')
+
+# =========================
 # FRANCES
+# =========================
 
 @app.route('/menu_fr')
 def menu_fr():
     return render_template('menu_fr.html')
 
+@app.route('/regionales_fr')
+def regionales_fr():
+    return render_template('regionales_fr.html')
+
+@app.route('/tradicionales_fr')
+def tradicionales_fr():
+    return render_template('tradicionales_fr.html')
+
+@app.route('/pastas_fr')
+def pastas_fr():
+    return render_template('pastas_fr.html')
+
 @app.route('/entrees_fr')
 def entrees_fr():
     return render_template('entrees_fr.html')
+
+@app.route('/postres_fr')
+def postres_fr():
+    return render_template('postres_fr.html')
+
+@app.route('/menu-dia_fr')
+def menu_dia_fr():
+    return render_template('menu-dia_fr.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
